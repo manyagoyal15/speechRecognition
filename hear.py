6466f44b3fe449732763ef3fb31847dd42f86ad5
Manya Goyal <43989106+manyagoyal15@users.noreply.github.com>
2020-10-29 17:04:58 +0530

    Add files via upload

--- hear.py
+++ hear.py
@@ -7,7 +7,7 @@ Created on Fri Aug 28 18:18:24 2020
 
 import speech_recognition as sr 
 import pyttsx3  
-  
+from textblob import TextBlob
 # Initialize the recognizer  
 r = sr.Recognizer()  
   
@@ -17,6 +17,16 @@ def SpeakText(command):
     engine = pyttsx3.init() 
     engine.say(command)  
     engine.runAndWait() 
+def analyze(command):
+    blob=TextBlob(command)
+    print(blob.sentiment)
+    if(blob.sentiment.polarity<0):
+        print("Negative thoughts!")
+    elif(blob.sentiment.polarity>0):
+        print("Positive thoughts!")
+    else:
+        print("Neutral statement")
+print("Welcome to the emotions test")
 while(1):     
       
     # Exception handling to handle 
@@ -29,7 +39,7 @@ while(1):
             # wait for a second to let the recognizer 
             # adjust the energy threshold based on 
             # the surrounding noise level
-            print("enter command")
+            print("what would you like to say pal?")
             r.adjust_for_ambient_noise(source2, duration=0.2) 
               
             #listens for the user's input  
@@ -41,6 +51,13 @@ while(1):
   
             print("Did you say "+MyText) 
             SpeakText(MyText) 
+            analyze(MyText)
+        ch=input("would you like to continue? (y/n)")
+        if(ch=="y" or ch=="Y"):
+            continue
+        else:
+            print("Thank you for taking the emotions test ")
+            break
     except sr.RequestError as e: 
         print("Could not request results; {0}".format(e)) 
           
